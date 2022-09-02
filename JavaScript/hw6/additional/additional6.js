5c68d89a948c2b7374f29ef1351efa5746dc32a4
man1fest000 <lubuskijurij@gmail.com>
2022-09-02 20:09:08 +0300

    exercise solutions

--- JavaScript/hw6/additional/additional6.js
+++ JavaScript/hw6/additional/additional6.js
@@ -5,7 +5,7 @@ console.log("наслаждение".split(""));
 function cutString(str, n) {
     let arr = [];
     for (let i = 0; i < str.length; i += 3) {
-        arr.push(str.substring(i, n));
+        arr.push(str.substr(i, n));
     }
     return arr;
 }
@@ -40,28 +40,64 @@ function insert_dash(str) {
 console.log(insert_dash(str2));
 
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
-//
+
+function upperFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(upperFirstLetter("html"));
+
 // - Дано список імен.
-//     let n1 = 'Harry..Potter'
-// let n2 = 'Ron---Whisley'
-// let n3 = 'Hermione__Granger'
+let n1 = "Harry..Potter";
+let n2 = "Ron---Whisley";
+let n3 = "Hermione__Granger";
 // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
 // let n1 = 'Harry Potter'
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
+
+function walidName(str) {
+    return str.replace(/\W+/g, " ");
+}
+
+console.log(walidName(n2));
+
 //
 //     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
-//
+
+function rundomArr(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * 101));
+    }
+    return arr;
+}
+
+console.log(rundomArr(30));
+
+console.log(rundomArr(30).sort((a, b) => a - b));
+
+console.log(rundomArr(30).filter((item) => item % 2 === 0));
+
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+function capitalize(str) {
+    return str
+        .split(" ")
+        .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+        .join(" ");
+}
+
+console.log(capitalize("hello world"));
+
 // - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
 // Протестувати на значеннях
-// someemail@gmail.com
+//someemail@gmail.com
 // someeMAIL@gmail.com
 // someeMAIL@i.ua
 // some.email@gmail.com
-//
+
 // Примітка
 // Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
 //
